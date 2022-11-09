0a8d7a94260d455b71a58f08359079104436f9ae
Samuel Tremblay <samuel.tremblay65@gmail.com>
2022-11-09 18:58:34 -0500

    Added create jiraf dialog

--- client/src/JirafItem.js
+++ client/src/JirafItem.js
@@ -4,12 +4,31 @@ export default function JirafItem({jirafItem}) {
     var tmpClassName;
     if(jirafItem.color != null) tmpClassName = "jirafItem " + jirafItem.color;
     else tmpClassName = "jirafItem";
+
+    var contentItems = [];
+
+    formatMessage();
+
+    function formatMessage()
+    {
+        var contentLines = jirafItem.message.split("\n");
+        contentLines.forEach(element => {
+            if(element.trim() === "")
+            {
+                contentItems.push(<br></br>);
+            }
+            else
+            {
+                contentItems.push(<p key="element">{element}</p>);
+            }
+        });
+    }
      
   return (
     <div className={tmpClassName}>
       <h1>{jirafItem.title}</h1>
       <p>{jirafItem.subtitle}</p>
-      <p>{jirafItem.message}</p>
+      <ul>{contentItems}</ul>
     </div>
   )
 }
